771694fbd3290f8dac10d43761e2cd39cf1b2f8f
Adrian Webb <adrian@webb.sh>
2024-09-16 18:12:05 -0400

    Updates to the bootstrap test script.

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -5,9 +5,23 @@
 #
 set -e
 
+# Initialize top level directories and load bootstrap functions
+SCRIPT_PATH="${BASH_SOURCE[0]}" # bash
+if [[ -z "$SCRIPT_PATH" ]]; then
+  SCRIPT_PATH="${(%):-%N}" # zsh
+fi
+
+export __test_dir="$(cd "$(dirname "${SCRIPT_PATH}")" && pwd)"
+export __project_dir="${__test_dir}/project"
+export __reactor_dir="$(dirname "${__test_dir}")"
+export __script_dir="${__reactor_dir}/scripts"
+
 # Verify intallation of required executables
 which python3
 which docker
 which git
 which curl
 which openssl
+
+# Install Python packages
+python3 -m pip install -r "${__reactor_dir}/requirements.txt"
