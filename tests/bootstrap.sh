094ecd93f4880403d8a7021d72d02e8edfc46752
Adrian Webb <adrian@webb.sh>
2024-09-16 18:37:31 -0400

    Updates to the bootstrap test script.

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -32,6 +32,6 @@ which openssl
 
 
 # Install Python packages
-pip install --upgrade pip
+pip install --upgrade pip setuptools wheel
 pip install "cython<3.0.0"
-pip install --no-build-isolation -r "${__reactor_dir}/requirements.txt"
+pip install -r "${__reactor_dir}/requirements.txt"
