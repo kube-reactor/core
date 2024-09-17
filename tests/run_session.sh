37664d40f98f6619c8df3398a61738b209279bb2
Adrian Webb <adrian@webb.sh>
2024-09-17 15:21:49 -0400

    Updates to the test run_session script.

--- tests/run_session.sh
+++ tests/run_session.sh
@@ -16,13 +16,80 @@ export __project_dir="${__test_dir}/project"
 export __reactor_dir="$(dirname "${__test_dir}")"
 export __script_dir="${__reactor_dir}/scripts"
 
-export PATH="${__script_dir}:$PATH"
-
 echo "Test directory: ${__test_dir}"
 echo "Project directory: ${__project_dir}"
 echo "Reactor directory: ${__reactor_dir}"
 echo "Script directory: ${__script_dir}"
 
+export PATH="${__script_dir}:$PATH"
 cd "${__project_dir}"
 
-reactor up --build --debug --no-cache
+#=========================================================================================
+# Development Session
+#
+
+#
+# Starting Up
+#
+reactor up --build --debug
+
+if [[ ! -d "./cache" ]] \
+  || [[ ! -d "./certs" ]] \
+  || [[ ! -d "./docker" ]] \
+  || ! git --work-tree=./docker/zimagi status 1>/dev/null 2>&1 \
+  || [[ ! -d "./charts" ]] \
+  || ! git --work-tree=./charts/zimagi status 1>/dev/null 2>&1 \
+  || [[ ! -d "./.minikube" ]] \
+  || [[ ! -f "./env/local/.kubeconfig" ]] \
+  || [[ ! -f "./logs/tunnel.kpid" ]] \
+  || [[ ! -f "./logs/dashboard.kpid" ]] \
+  || [[ ! -d "./.terraform" ]] \
+  || [[ ! -f "./terraform/argocd-apps/gateway/.terraform.lock.hcl" ]] \
+  || [[ ! -f "./terraform/argocd-apps/gateway/terraform.tfstate" ]] \
+  || [[ ! -f "./logs/hosts.txt" ]] \
+  || ! cat /etc/hosts | grep test 1>/dev/null 2>/dev/null; then
+  echo "Reactor up failed with missing files"
+  exit 1
+fi
+
+#
+# Running Command Tests
+#
+for file in "${__test_dir}/commands"/*.sh; do
+  echo "$file"
+done
+
+#
+# Shutting Down
+#
+reactor down --debug
+
+if [[ -f "./env/local/.kubeconfig" ]] \
+  || [[ -f "./logs/tunnel.kpid" ]] \
+  || [[ -f "./logs/dashboard.kpid" ]] \
+  || [[ -f "./logs/hosts.txt" ]] \
+  || cat /etc/hosts | grep test; then
+  echo "Reactor down failed with remaining files"
+  exit 1
+fi
+
+#
+# Cleaning Up
+#
+reactor destroy --force --debug
+
+if [[ -d "./.minikube" ]] \
+  || [[ -d "./.terraform" ]] \
+  || [[ -f "./terraform/argocd-apps/gateway/.terraform.lock.hcl" ]] \
+  || [[ -f "./terraform/argocd-apps/gateway/terraform.tfstate" ]]; then
+  echo "Reactor destroy failed with remaining files"
+  exit 1
+fi
+
+reactor clean --force --debug
+
+if [[ -d "./cache" ]] \
+  || [[ -d "./certs" ]]; then
+  echo "Reactor clean failed with remaining files"
+  exit 1
+fi
