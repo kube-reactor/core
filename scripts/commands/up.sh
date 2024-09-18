334d27264afbc500f6a75ccb4bcc98b7e71ab0e2
Adrian Webb <adrian@webb.sh>
2024-09-18 19:16:12 -0400

    Moving the download of required binary executables in the up command.

--- scripts/commands/up.sh
+++ scripts/commands/up.sh
@@ -86,7 +86,26 @@ function up_environment () {
 function up_command () {
   up_environment "$@"
   cert_environment
+  minikube_environment
   helm_environment
+
+  info "Downloading local software dependencies ..."
+  download_binary minikube \
+    "https://storage.googleapis.com/minikube/releases/latest/minikube-${__os}-${__architecture}" \
+    "${__binary_dir}"
+
+  download_binary kubectl \
+    "https://dl.k8s.io/release/v${KUBECTL_VERSION}/bin/linux/${__architecture}/kubectl" \
+    "${__binary_dir}"
+
+  download_binary helm \
+    "https://get.helm.sh/helm-v${HELM_VERSION}-${__os}-${__architecture}.tar.gz" \
+    "${__binary_dir}" \
+    "${__os}-${__architecture}"
+
+  download_binary argocd \
+    "https://github.com/argoproj/argo-cd/releases/latest/download/argocd-${__os}-${__architecture}" \
+    "${__binary_dir}"
 
   info "Generating ingress certificates ..."
   generate_certs \
@@ -108,26 +127,6 @@ function up_command () {
 
 function up_host_command () {
   up_environment "$@"
-  minikube_environment
-  helm_environment
-
-  info "Downloading local software dependencies ..."
-  download_binary minikube \
-    "https://storage.googleapis.com/minikube/releases/latest/minikube-${__os}-${__architecture}" \
-    "${__binary_dir}"
-
-  download_binary kubectl \
-    "https://dl.k8s.io/release/v${KUBECTL_VERSION}/bin/linux/${__architecture}/kubectl" \
-    "${__binary_dir}"
-
-  download_binary helm \
-    "https://get.helm.sh/helm-v${HELM_VERSION}-${__os}-${__architecture}.tar.gz" \
-    "${__binary_dir}" \
-    "${__os}-${__architecture}"
-
-  download_binary argocd \
-    "https://github.com/argoproj/argo-cd/releases/latest/download/argocd-${__os}-${__architecture}" \
-    "${__binary_dir}"
 
   launch_host_minikube_tunnel
   launch_host_minikube_dashboard
