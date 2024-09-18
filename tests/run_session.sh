1aa2fcd3402af20f55ea904e3a7e0424e727629e
Adrian Webb <adrian@webb.sh>
2024-09-18 19:31:23 -0400

    Updating the bootstrap and run_session test script shebang and script path assignments.

--- tests/run_session.sh
+++ tests/run_session.sh
@@ -1,12 +1,10 @@
+#!/usr/bin/env bash
 #
 #=========================================================================================
 # Initialization
 #
 # Initialize top level directories and load bootstrap functions
 SCRIPT_PATH="${BASH_SOURCE[0]}" # bash
-if [[ -z "$SCRIPT_PATH" ]]; then
-  SCRIPT_PATH="${(%):-%N}" # zsh
-fi
 
 export __test_dir="$(cd "$(dirname "${SCRIPT_PATH}")" && pwd)"
 export __project_dir="${__test_dir}/project"
