f273edb1b0a7a2197d53c3b3ca937b79f4c1a85a
Adrian Webb <adrian@webb.sh>
2024-09-17 21:03:05 -0400

    Debugging related changes to the CircleCI configuration and bootstrap test script.

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -16,10 +16,27 @@ export __project_dir="${__test_dir}/project"
 export __reactor_dir="$(dirname "${__test_dir}")"
 export __script_dir="${__reactor_dir}/scripts"
 
+# Set OS and system architecture variables.
+case "$OSTYPE" in
+  darwin*) __os="darwin" ;;
+  linux*) __os="linux" ;;
+  *) echo "Unsupported OS: $OSTYPE"; exit 1 ;;
+esac
+export __os
+
+case $(uname -m) in
+    x86_64 | amd64) __architecture="amd64" ;;
+    aarch64 | arm64) __architecture="arm64" ;;
+    *) echo "Unsupported CPU architecture: $(uname -m)"; exit 1 ;;
+esac
+export __architecture
+
 echo "Test directory: ${__test_dir}"
 echo "Project directory: ${__project_dir}"
 echo "Reactor directory: ${__reactor_dir}"
 echo "Script directory: ${__script_dir}"
+echo "Operating System: ${__os}"
+echo "Computing Architecture: ${__architecture}"
 
 # Verify intallation of required executables
 which python3
@@ -29,5 +46,10 @@ which curl
 which openssl
 
 # Install Python packages
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install --no-build-isolation -r "${__reactor_dir}/requirements.txt"
+if [ "${__os}" == "darwin" ]; then
+  python3 -m pip install --upgrade pip setuptools wheel
+  python3 -m pip install -r "${__reactor_dir}/requirements.txt"
+else
+  python3 -m pip install --upgrade pip setuptools wheel
+  python3 -m pip install --no-build-isolation -r "${__reactor_dir}/requirements.txt"
+fi
