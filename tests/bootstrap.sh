20c89a7907ffea972c1f63196ca91b778b26fb92
Adrian Webb <adrian@webb.sh>
2024-09-21 17:40:56 -0400

    Testing the CircleCI configuration.

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -46,7 +46,9 @@ which curl
 which openssl
 
 # Install Python packages
-python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install --no-build-isolation \
+  --upgrade pip setuptools wheel
+
 python3 -m pip install --no-build-isolation \
   --global-option="--install-scripts=${__script_dir}" \
   --requirement "${__reactor_dir}/requirements.txt"
