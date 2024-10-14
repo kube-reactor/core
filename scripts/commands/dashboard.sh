1475b379449d5fd50c7bde7d55312d9c7f8379c2
Adrian Webb <adrian@webb.sh>
2024-10-14 00:05:38 -0400

    Updating the reactor-exec executable to not require docker command if host_command is defined and updating the dashboard command.

--- scripts/commands/dashboard.sh
+++ scripts/commands/dashboard.sh
@@ -7,10 +7,9 @@ function dashboard_description () {
   echo "Launch the Kubernetes Dashboard for the Minikube cluster"
 }
 
-function dashboard_command () {
-  debug "No operation"
-}
-
 function dashboard_host_command () {
+  if ! minikube_status; then
+    emergency "Minikube is not running"
+  fi
   launch_host_minikube_dashboard
 }
