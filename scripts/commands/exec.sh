c4598e1c8872822d7e8986ffd0374fcc1e162c2d
Adrian Webb <adrian@webb.sh>
2024-10-21 05:01:04 -0400

    Fixing various issues with the exec command.

--- scripts/commands/exec.sh
+++ scripts/commands/exec.sh
@@ -4,11 +4,12 @@
 #
 
 function exec_description () {
-  render "Launch a service within the Kubernetes cluster"
+  render "Execute and watch a temporary command within the Kubernetes cluster"
 }
 
 function exec_command_environment () {
   namespace_option
+  wait_option "for container creation" 3
   service_image_arg
   required_command_args
 }
@@ -20,23 +21,38 @@ function exec_command () {
     emergency "Minikube is not running"
   fi
 
-  render "$SERVICE_NAMESPACE"
-  render "$SERVICE_IMAGE"
-  render "${SERVICE_COMMAND[@]}"
+  POD_NAME="$(date +%Y%m%d%H%M%S)"
+  BASE_ARGS=(
+    "-n" "$SERVICE_NAMESPACE"
+    "$POD_NAME"
+  )
+  COMMAND_ARGS=(
+    "${BASE_ARGS[@]}"
+    "--restart" "Never"
+    "--image" "$SERVICE_IMAGE"
+    "--command" "--" ${SERVICE_COMMAND[@]}
+  )
+  LOG_ARGS=(
+    "${BASE_ARGS[@]}"
+    "--pod-running-timeout" "30s"
+    "--ignore-errors"
+    "--tail" 100
+    "--follow"
+    "--timestamps"
+  )
+  DELETE_ARGS=(
+    "${BASE_ARGS[@]}"
+    "--force"
+  )
 
+  debug "> BASE_ARGS: ${BASE_ARGS[@]}"
+  debug "> COMMAND_ARGS: ${COMMAND_ARGS[@]}"
+  debug "> LOG_ARGS: ${LOG_ARGS[@]}"
+  debug "> DELETE_ARGS: ${DELETE_ARGS[@]}"
 
-  # SERVICE_OPTIONS=(
-  #   "-n" "$SERVICE_NAMESPACE"
-  #   "--image" "$SERVICE_IMAGE"
-  # )
-  # COMMAND=()
-  # if [ "$SERVICE_COMMAND" ]; then
-  #   SERVICE_OPTIONS=("${SERVICE_OPTIONS[@]}" "-it" "--rm")
-  #   COMMAND=("${COMMAND[@]}" "--" "$SERVICE_COMMAND")
-  # fi
-  # "${__binary_dir}/kubectl" run \
-  #   "${SERVICE_OPTIONS[@]}" \
-  #   "$SERVICE_POD_NAME" \
-  #   "${COMMAND[@]}"
-  # add_space
+  "${__binary_dir}/kubectl" run "${COMMAND_ARGS[@]}"
+  sleep $WAIT
+  "${__binary_dir}/kubectl" logs "${LOG_ARGS[@]}"
+  "${__binary_dir}/kubectl" delete pod "${DELETE_ARGS[@]}" 2>/dev/null
+  add_space
 }
