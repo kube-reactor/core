07496e0134f7b2312ddba6c3ee3acc09ce7a22fa
Adrian Webb <adrian@webb.sh>
2024-11-12 01:59:32 -0500

    Updating the cookiecutter binary search in the create command.

--- bin/commands/create.sh
+++ bin/commands/create.sh
@@ -124,7 +124,7 @@ function create_command_environment () {
 
 function create_command () {
   echo "Searching for cookiecutter"
-  cookiecutter_bin="$(find ~ -name cookiecutter 2>/dev/null | grep -m 1 "/bin/")"
+  cookiecutter_bin="$(sudo find / -name cookiecutter 2>/dev/null | grep -m 1 "/bin/")"
   echo "$cookiecutter_bin"
   project_temp_dir="/tmp/reactor/download"
 
