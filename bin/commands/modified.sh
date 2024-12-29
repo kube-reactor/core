a8adb93a7d092efe756e67b9cf5d5d9ee285caed
Adrian Webb <adrian@webb.sh>
2024-12-29 18:51:44 -0500

    Updating the modified command to only display projects with changes to avoid clutter.

--- bin/commands/modified.sh
+++ bin/commands/modified.sh
@@ -9,29 +9,37 @@ function modified_description () {
 
 function modified_command () {
   if [ -d "${__project_dir}/.git" ]; then
-    add_line '-'
-    info "Checking $(variable_color "${__project_name}") Project repository"
-    info ""
-    info "Directory: $(variable_color "${__project_dir}")"
-    info ""
-    add_line '-'
     cd "${__project_dir}"
-    git status
+
+    if ! git status 2>&1 | grep "nothing to commit, working tree clean" >/dev/null 2>&1; then
+      add_line '-'
+      info "Checking $(variable_color "${__project_name}") Project repository"
+      info ""
+      info "Directory: $(variable_color "${__project_dir}")"
+      info ""
+      add_line '-'
+
+      git status
+    fi
   fi
 
   for project in $(config docker); do
     project_dir="${__docker_dir}/${project}"
 
     if [ -d "${project_dir}/.git" ]; then
-      add_space
-      add_line '-'
-      info "Checking $(variable_color "${project}") Docker image repository"
-      info ""
-      info "Directory: $(variable_color "${project_dir}")"
-      info ""
-      add_line '-'
       cd "$project_dir"
-      git status
+
+      if ! git status 2>&1 | grep "nothing to commit, working tree clean" >/dev/null 2>&1; then
+        add_space
+        add_line '-'
+        info "Checking $(variable_color "${project}") Docker image repository"
+        info ""
+        info "Directory: $(variable_color "${project_dir}")"
+        info ""
+        add_line '-'
+
+        git status
+      fi
     fi
   done
 
@@ -39,15 +47,19 @@ function modified_command () {
     chart_dir="${__charts_dir}/${chart}"
 
     if [ -d "${chart_dir}/.git" ]; then
-      add_space
-      add_line '-'
-      info "Checking $(variable_color "${chart}") Helm chart repository"
-      info ""
-      info "Directory: $(variable_color "${chart_dir}")"
-      info ""
-      add_line '-'
       cd "$chart_dir"
-      git status
+
+      if ! git status 2>&1 | grep "nothing to commit, working tree clean" >/dev/null 2>&1; then
+        add_space
+        add_line '-'
+        info "Checking $(variable_color "${chart}") Helm chart repository"
+        info ""
+        info "Directory: $(variable_color "${chart_dir}")"
+        info ""
+        add_line '-'
+
+        git status
+      fi
     fi
   done
 
@@ -55,15 +67,19 @@ function modified_command () {
     extension_dir="${__extension_dir}/${extension}"
 
     if [ -d "${extension_dir}/.git" ]; then
-      add_space
-      add_line '-'
-      info "Checking reactor $(variable_color "${extension}") Extension repository"
-      info ""
-      info "Directory: $(variable_color "${extension_dir}")"
-      info ""
-      add_line '-'
       cd "$extension_dir"
-      git status
+
+      if ! git status 2>&1 | grep "nothing to commit, working tree clean" >/dev/null 2>&1; then
+        add_space
+        add_line '-'
+        info "Checking reactor $(variable_color "${extension}") Extension repository"
+        info ""
+        info "Directory: $(variable_color "${extension_dir}")"
+        info ""
+        add_line '-'
+
+        git status
+      fi
     fi
   done
 
