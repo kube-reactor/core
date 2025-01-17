5bb7b5141e7b93fcb524d80ad686dbe601e39ed3
Adrian Webb <adrian@webb.sh>
2025-01-17 18:47:35 -0500

    Adding load_utilities function call after running build subcommand in the up command script.

--- bin/commands/up.sh
+++ bin/commands/up.sh
@@ -42,6 +42,7 @@ function up_command () {
 
   if [[ "$BUILD" ]] || [[ ! -f "${__init_file}" ]]; then
     run_subcommand build "${BUILD_ARGS[@]}"
+    load_utilities
   fi
   touch "${__init_file}"
   run_subcommand update
