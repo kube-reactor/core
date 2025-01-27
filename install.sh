02398d2e6c2e08d78c1320e03676bbddb76730d9
Adrian Webb <adrian@webb.sh>
2025-01-27 16:23:04 -0500

    Refactoring the dependency installation process and adding Dockerless execution for CI/CD.

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+set -e
+
+export __reactor_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export __bin_dir="${__reactor_dir}/bin"
+
+#
+# Collect and Check Dependencies
+#
+source "${__bin_dir}/core/loader"
+
+#
+# Collect and Check Dependencies
+#
+check_dependencies
+setup_installer
+
+#
+# Run Installer
+#
+./requirements/install.sh
+
+#
+# Cleanup Installation Files
+#
+clean_installer
