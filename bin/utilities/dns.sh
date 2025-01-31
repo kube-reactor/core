74e564edcd60af706d7a1c059c1640d70a1663d4
Adrian Webb <adrian@webb.sh>
2025-01-31 17:43:51 -0500

    Refactoring the DNS provisioning in the DNS utility library.

--- bin/utilities/dns.sh
+++ bin/utilities/dns.sh
@@ -11,11 +11,25 @@ fi
 
 function dns_environment () {
   debug "Setting DNS environment ..."
-  export HOSTS_FILE="${HOSTS_FILE:-"$DEFAULT_HOSTS_FILE"}"
-  export HOSTS_MANIFEST_FILE="$(logdir)/hosts.txt"
+  export DNS_PROVIDER="${DNS_PROVIDER:-host}"
 
-  debug "HOSTS_FILE: ${HOSTS_FILE}"
-  debug "HOSTS_MANIFEST_FILE: ${HOSTS_MANIFEST_FILE}"
+  debug "DNS_PROVIDER: ${DNS_PROVIDER}"
+
+  run_dns_function dns_environment
+}
+
+
+function run_dns_function () {
+  local base_name="$1"
+  shift
+
+  local provider_name="${base_name}_${DNS_PROVIDER}"
+  debug "Running DNS function: ${provider_name}"
+
+  if function_exists "$provider_name"; then
+    "$provider_name" "$@"
+  fi
+  return $?
 }
 
 
@@ -23,70 +37,40 @@ function dns_ip () {
   echo "$("${__bin_dir}/kubectl" get service nginx-nginx-ingress-controller -n nginx -o jsonpath='{.status.loadBalancer.ingress[*].ip}' 2>/dev/null)"
 }
 
+function dns_hostname () {
+  echo "$("${__bin_dir}/kubectl" get service nginx-nginx-ingress-controller -n nginx -o jsonpath='{.status.loadBalancer.ingress[*].hostname}' 2>/dev/null)"
+}
+
+function dns_endpoint () {
+  local ip="$(dns_ip)"
+  if [ ! "$ip" ]; then
+    ip="$(dns_hostname)"
+  fi
+  echo "$ip"
+}
+
 function dns_hosts () {
   echo "$("${__bin_dir}/kubectl" get ingress -A -o jsonpath='{.items[*].spec.rules[*].host}' 2>/dev/null)"
 }
 
-function dns_records () {
-  dns_map=("###! $APP_NAME DNS MAP !###")
 
-  for host in $(dns_hosts); do
-    for ip in $(dns_ip); do
-      dns_map=("${dns_map[@]}" "$ip $host")
-    done
-  done
-
-  dns_map=("${dns_map[@]}" "###! END $APP_NAME DNS MAP !###")
-  dns_map="$(printf "%s\n" "${dns_map[@]}")"
-  echo "$dns_map"
-}
-
-
-function create_host_dns_records () {
-  # Runs on host machine (must be run after Kubernetes tunnel created)
+function save_dns_records () {
   dns_environment
 
   info "Waiting on Nginx ingress to initialize ..."
   while true; do
-    dns_ip="$(dns_ip)"
-    if [ ! -z "$dns_ip" ]; then
+    dns_endpoint="$(dns_endpoint)"
+    if [ ! -z "$dns_endpoint" ]; then
       break
     fi
     sleep 5
   done
 
-  info "Saving DNS records (hosts.txt):"
-  dns_records="$(dns_records)"
-  printf "$dns_records" | sudo tee "$HOSTS_MANIFEST_FILE" >/dev/null 2>&1
-}
-
-function save_host_dns_records () {
-  # Runs on host machine (requires sudo)
-  dns_environment
-
-  if [ -f "${HOSTS_MANIFEST_FILE:-}" ]; then
-    remove_host_dns_records
-
-    dns_records="$(cat "$HOSTS_MANIFEST_FILE")"
-
-    info "Saving DNS records (requires sudo):"
-    info "$dns_records"
-    printf "\n$dns_records" | sudo tee -a "$HOSTS_FILE" >/dev/null 2>&1
-  fi
+  run_dns_function save_dns_records
 }
 
 
 function remove_dns_records () {
   dns_environment
-  rm -f "$HOSTS_MANIFEST_FILE"
-}
-
-function remove_host_dns_records () {
-  # Runs on host machine (requires sudo)
-  dns_environment
-
-  if [ -f "${HOSTS_FILE:-}" ]; then
-    info "Removing existing DNS records (requires sudo)"
-    sudo perl -i -p0e "s/\n\#\#\#\!\s${APP_NAME}\sDNS\sMAP\s\!\#\#\#.+\#\#\#\!\sEND\s${APP_NAME}\sDNS\sMAP\s\!\#\#\#//se" "$HOSTS_FILE"
-  fi
+  run_dns_function remove_dns_records
 }
