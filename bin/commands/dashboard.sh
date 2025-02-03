db99c17986bf092311467468c4010483d88fe372
Adrian Webb <adrian@webb.sh>
2025-02-03 04:36:59 -0500

    Adding Kubernetes dashboard serviceaccount token generation in the dashboard command.

--- bin/commands/dashboard.sh
+++ bin/commands/dashboard.sh
@@ -11,5 +11,19 @@ function dashboard_host_command () {
   if ! kubernetes_status; then
     emergency "Kubernetes is not running"
   fi
-  launch_host_kubernetes_dashboard
+  if [ "$REACTOR_ENVIRONMENT" == "local" ]; then
+    launch_host_kubernetes_dashboard
+  else
+    if ! "${__bin_dir}/kubectl" describe serviceaccount -n kube-system admin-user 1>/dev/null 2>&1; then
+      "${__bin_dir}/kubectl" create serviceaccount -n kube-system admin-user
+    fi
+    if ! "${__bin_dir}/kubectl" describe clusterrolebinding -n kube-system dashboard-admin 1>/dev/null 2>&1; then
+      "${__bin_dir}/kubectl" create clusterrolebinding -n kube-system dashboard-admin --clusterrole=cluster-admin --serviceaccount=kube-system:admin-user
+    fi
+
+    info "Use the following token to login to the dashboard:"
+    add_space
+    "${__bin_dir}/kubectl" create token -n kube-system admin-user
+    add_space
+  fi
 }
