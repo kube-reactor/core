5ac68531153a6384f09365777b0abc255f61a3c8
Adrian Webb <adrian@webb.sh>
2025-02-19 02:17:47 -0500

    Adding more temporary debugging statements in the core install library.

--- bin/core/lib/install.sh
+++ bin/core/lib/install.sh
@@ -6,21 +6,37 @@
 #
 
 function install_debian () {
+  echo "-1-1"
   sudo apt-get update -y 1>>"$(logfile)" 2>&1
+
+  echo "-1-2"
 
   sed '/^\s*\#.*$/d' "${__reactor_dir}/requirements/packages_debian.txt" \
     | xargs -r sudo apt-get install -y --no-install-recommends 1>>"$(logfile)" 2>&1
 
+  echo "-1-3"
+
   if ! which terraform 1>/dev/null 2>&1; then
+    echo "-1-4"
+
     wget -O - https://apt.releases.hashicorp.com/gpg \
       | sudo gpg --yes --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg 1>>"$(logfile)" 2>&1
+
+    echo "-1-5"
 
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
       https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
       | sudo tee /etc/apt/sources.list.d/hashicorp.list 1>>"$(logfile)" 2>&1
 
+    echo "-1-6"
+
     sudo apt-get update -y 1>>"$(logfile)" 2>&1
+
+    echo "-1-7"
+
     sudo apt-get install -y terraform 1>>"$(logfile)" 2>&1
+
+    echo "-1-8"
   fi
 }
 
