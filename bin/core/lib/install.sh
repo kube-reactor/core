effc194deea7bde44f6c3395e1d9ed84ea5bf90c
Adrian Webb <adrian@webb.sh>
2025-02-19 03:26:06 -0500

    Reordering the Mac installation dependencies and modifying the brew installation procedure.

--- bin/core/lib/install.sh
+++ bin/core/lib/install.sh
@@ -42,10 +42,18 @@ function install_debian () {
 }
 
 function install_mac () {
-  check_binary brew 1>>"$(logfile)" 2>&1
-
   if ! check_binary brew 1>>"$(logfile)" 2>&1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+  brew update 1>>"$(logfile)" 2>&1
+
+  if [ "${__bash_version}" -lt "4" ]; then
+    echo "Reactor requires Bash version 4+"
+    echo "Upgrading Bash version ..."
+    brew install bash 1>>"$(logfile)" 2>&1
+    echo ""
+    echo "Your Bash version has been upgraded.  Please rerun this command"
+    exit 0
   fi
 
   brew install openssl 1>>"$(logfile)" 2>&1
@@ -57,14 +65,5 @@ function install_mac () {
   if ! check_binary terraform 1>>"$(logfile)" 2>&1; then
     brew tap hashicorp/tap 1>>"$(logfile)" 2>&1
     brew install hashicorp/tap/terraform 1>>"$(logfile)" 2>&1
-  fi
-
-  if [ "${__bash_version}" -lt "4" ]; then
-    echo "Reactor requires Bash version 4+"
-    echo "Upgrading Bash version ..."
-    brew install bash 1>>"$(logfile)" 2>&1
-    echo ""
-    echo "Your Bash version has been upgraded.  Please rerun this command"
-    exit 0
   fi
 }
