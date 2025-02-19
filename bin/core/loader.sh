10427ba56023d9716ea239d60715cc6e08cf4a68
Adrian Webb <adrian@webb.sh>
2025-02-19 02:06:49 -0500

    Updating the base cluster project template URL in the core loader script.

--- bin/core/loader.sh
+++ bin/core/loader.sh
@@ -121,7 +121,7 @@ if [ -z "${HOME_SHARES:-}" ]; then
   )
 fi
 
-export DEFAULT_PROJECT_URL="${DEFAULT_REACTOR_URL:-https://github.com/kube-reactor/cluster-base.git}"
+export DEFAULT_PROJECT_URL="${DEFAULT_REACTOR_URL:-https://github.com/kube-reactor/cluster-base-aws.git}"
 export DEFAULT_PROJECT_REMOTE="${DEFAULT_REACTOR_REMOTE:-origin}"
 export DEFAULT_PROJECT_REFERENCE="${DEFAULT_REACTOR_REFERENCE:-main}"
 
