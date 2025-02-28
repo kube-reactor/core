7d5af6d67e245375ad1001c789f427b8d2fdc0ce
Adrian Webb <adrian@webb.sh>
2025-02-28 00:25:43 -0500

    Adding the GitHub CLI tool to the Debian and MacOS operating systems in the core install script.

--- bin/core/lib/install.sh
+++ bin/core/lib/install.sh
@@ -39,10 +39,22 @@ function install_debian () {
     sudo apt-get update -y 1>>"$(logfile)" 2>&1
     sudo apt-get install -y terraform 1>>"$(logfile)" 2>&1
   fi
+
+  if ! which gh 1>/dev/null 2>&1; then
+    wget -O - https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+      | sudo gpg --yes --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg 1>>"$(logfile)" 2>&1
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] \
+      https://cli.github.com/packages stable main" \
+      | sudo tee /etc/apt/sources.list.d/github-cli.list 1>>"$(logfile)" 2>&1
+
+    sudo apt-get update -y 1>>"$(logfile)" 2>&1
+    sudo apt-get install -y gh 1>>"$(logfile)" 2>&1
+  fi
 }
 
 function install_mac () {
-  if ! which brew 1>>"$(logfile)" 2>&1; then
+  if ! which brew 1>/dev/null 2>&1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
   brew update 1>>"$(logfile)" 2>&1
@@ -59,11 +71,14 @@ function install_mac () {
   brew install openssl 1>>"$(logfile)" 2>&1
   brew install grep 1>>"$(logfile)" 2>&1
 
-  if ! which python3 1>>"$(logfile)" 2>&1; then
+  if ! which python3 1>/dev/null 2>&1; then
     brew install python 1>>"$(logfile)" 2>&1
   fi
-  if ! which terraform 1>>"$(logfile)" 2>&1; then
+  if ! which terraform 1>/dev/null 2>&1; then
     brew tap hashicorp/tap 1>>"$(logfile)" 2>&1
     brew install hashicorp/tap/terraform 1>>"$(logfile)" 2>&1
+  fi
+  if ! which gh 1>/dev/null 2>&1; then
+    brew install gh 1>>"$(logfile)" 2>&1
   fi
 }
