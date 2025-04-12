bf75be8b083a84c76a1735c8b5509abb2b3139f1
Adrian Webb <adrian@webb.sh>
2025-04-12 04:56:31 -0400

    Removing uneeded functions and updating the init_loader function implementation in the core runtime library.

--- bin/core/lib/runtime.sh
+++ bin/core/lib/runtime.sh
@@ -4,7 +4,11 @@ function init_loader () {
   export COMMAND_INDEX=()
   export UTILITY_INDEX=()
 
-  update_projects
+  if [ ! -f "${__library_file}" ]; then
+    update_projects
+  else
+    load_libraries
+  fi
 
   if ! is_setup_complete; then
     install_os_requirements
@@ -27,51 +31,10 @@ function load_hook () {
 }
 
 
-function load_utility () {
-  local utility_name="$1"
-  if [[ "${2:-}" ]] || [[ ! " ${UTILITY_INDEX[*]} " =~ [[:space:]]${utility_name}[[:space:]] ]]; then
-    source_utility "$utility_name"
-    export UTILITY_INDEX=("${UTILITY_INDEX[@]}" "$utility_name")
-  fi
-}
-
-function load_utilities () {
-  if [ $# -gt 0 ]; then
-    for library in $@; do
-      load_utility "$library"
-    done
-  else
-    load_library utilities
-    run_hook initialize_utilities
-  fi
-}
-
-
-function load_command () {
- local command_name="$1"
-  if [[ "${2:-}" ]] || [[ ! " ${COMMAND_INDEX[*]} " =~ [[:space:]]${command_name}[[:space:]] ]]; then
-    source "${__commands_dir}/${command_name}.sh"
-    export COMMAND_INDEX=("${COMMAND_INDEX[@]}" "$command_name")
-  fi
-}
-
-function load_commands () {
-  if [ $# -gt 0 ]; then
-    for command in $@; do
-      load_command "$command"
-    done
-  else
-    load_library commands
-    run_hook initialize_commands
-  fi
-}
-
-
 function parse_cli () {
   local param_function="$1"
   shift
 
-  load_utilities help
   set_initialized
   reactor_args "$@"
 
