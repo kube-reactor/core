0d368daa8a1faa676cba3ff87178897cd2fe9159
Adrian Webb <adrian@webb.sh>
2025-05-22 00:09:05 -0400

    Adding preprocessing of core arguments in the core environment script to ensure they are available during bootstrap process.

--- bin/core/env.sh
+++ bin/core/env.sh
@@ -6,6 +6,7 @@
 #
 #  > Optional: ${__bin_dir}
 #  > Optional: $SOURCED => [ 1, 0 ]
+#  > App Arguments: ${__app_args}
 #
 #=========================================================================================
 # Initialization
@@ -19,6 +20,28 @@ fi
 #
 # Loading environment
 #
+# Need to preparse these because option parser is not run yet
+#
+# See: utilities/args.sh -> reactor_args
+
+export arg_n=""
+export arg_v=""
+export arg_d=""
+export arg_r=""
+
+if [[ " ${__app_args[*]} " =~ " -n " ]] || [[ " ${__app_args[*]} " =~ " --no-color " ]]; then
+    export arg_n="1"
+fi
+if [[ " ${__app_args[*]} " =~ " -v " ]] || [[ " ${__app_args[*]} " =~ " --verbose " ]]; then
+    export arg_v="1"
+fi
+if [[ " ${__app_args[*]} " =~ " -d " ]] || [[ " ${__app_args[*]} " =~ " --debug " ]]; then
+    export arg_d="1"
+fi
+if [[ " ${__app_args[*]} " =~ " -r " ]] || [[ " ${__app_args[*]} " =~ " --reload " ]]; then
+    export arg_r="1"
+fi
+
 source "${__bin_dir}/core/loader.sh"
 load_hook initialize
 
