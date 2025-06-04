05b9797012a4a2d264d87e4b0ed44d969d187d1f
Adrian Webb <adrian@webb.sh>
2025-06-03 21:23:00 -0400

    Removing the sleep before saving DNS records in the update command.

--- bin/commands/update.sh
+++ bin/commands/update.sh
@@ -74,9 +74,6 @@ function update_command () {
   launch_kubernetes_tunnel
 
   if [ "$UPDATE_ALL" -o "$UPDATE_DNS" ]; then
-    if [ "$UPDATE_ALL" ]; then
-      sleep 60
-    fi
     save_dns_records
   fi
   if [ "$UPDATE_ALL" -o "$UPDATE_CHARTS" ]; then
