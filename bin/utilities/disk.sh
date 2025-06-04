ffb3f3e2bba664bdeec82baa8269145c1fe51369
Adrian Webb <adrian@webb.sh>
2025-06-03 21:12:51 -0400

    Updating the git update process to fix branch merges after fetch in the disk utility library.

--- bin/utilities/disk.sh
+++ bin/utilities/disk.sh
@@ -38,13 +38,10 @@ function clean_cache () {
 # Git Utilities
 #
 
-function exec_git ()
-{
+function exec_git () {
   DIRECTORY="$1";
   shift;
-  if [ ! "${REACTOR_SHELL_OUTPUT:-}" ]; then
-    echo "Running git ${@} on ${DIRECTORY}" >>"$(logfile)"
-  fi
+  echo "Running git ${@} on ${DIRECTORY}" >>"$(logfile)"
   git --git-dir="${DIRECTORY}/.git" --work-tree="${DIRECTORY}" "$@" 1>>"$(logfile)" 2>&1
 }
 
@@ -61,12 +58,11 @@ function download_git_repo () {
     info "Fetching repository \"$URL\" into folder \"$DIRECTORY\" ..."
     git clone --quiet "$URL" "$DIRECTORY" 1>>"$(logfile)" 2>&1
   fi
-  exec_git "$DIRECTORY" fetch origin --tags
+  exec_git "$DIRECTORY" fetch origin --tags 1>>"$(logfile)" 2>&1
+  exec_git "$DIRECTORY" checkout "$REFERENCE" -- 1>>"$(logfile)" 2>&1
 
-  if [ ! "${REACTOR_SHELL_OUTPUT:-}" ]; then
-    exec_git "$DIRECTORY" checkout "$REFERENCE" -- 1>>"$(logfile)" 2>&1
-  else
-    exec_git "$DIRECTORY" checkout "$REFERENCE" -- 1>/dev/null 2>&1
+  if exec_git "$DIRECTORY" show-ref --verify "refs/heads/${REFERENCE}" >/dev/null 2>&1; then
+    exec_git "$DIRECTORY" merge "$REFERENCE" 1>>"$(logfile)" 2>&1
   fi
 }
 
