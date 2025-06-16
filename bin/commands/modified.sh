27a277b9f91654233a1b3abd6fc9ac3359e1711b
Adrian Webb <adrian@webb.sh>
2025-06-16 02:29:25 -0400

    Updating the modified command to support single root directory docker, chart, and extension projects.

--- bin/commands/modified.sh
+++ bin/commands/modified.sh
@@ -11,14 +11,31 @@ function modified_command () {
   check_git_status "project" "${__project_name}" "${__project_dir}"
   check_git_status "provisioner" "argocd-apps" "${__argocd_apps_dir}"
 
-  for project in $(config docker); do
-    check_git_status "docker" "${project}" "${__docker_dir}/${project}"
+  declare -A processed
+
+  for docker in $(config docker); do
+    project_reference="$(config docker.$docker.project $docker)"
+    project_dir="${__repo_dir}/${project_reference}"
+    if [[ -z "${processed["$project_dir"]}" ]]; then
+      check_git_status "docker" "${docker}" "${project_dir}"
+    fi
+    processed["$project_dir"]=1
   done
   for chart in $(config charts); do
-    check_git_status "chart" "${chart}" "${__charts_dir}/${chart}"
+    project_reference="$(config charts.$chart.project $chart)"
+    project_dir="${__repo_dir}/${project_reference}"
+    if [[ -z "${processed["$project_dir"]}" ]]; then
+      check_git_status "chart" "${chart}" "${project_dir}"
+    fi
+    processed["$project_dir"]=1
   done
   for extension in $(config extensions); do
-    check_git_status "extension" "${extension}" "${__extension_dir}/${extension}"
+    project_reference="$(config extensions.$extension.project $extension)"
+    project_dir="${__repo_dir}/${project_reference}"
+    if [[ -z "${processed["$project_dir"]}" ]]; then
+      check_git_status "extension" "${extension}" "${project_dir}"
+    fi
+    processed["$project_dir"]=1
   done
 
   run_hook modified
